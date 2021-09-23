adf955a8fe00961444dd6ec3e45919ea1eaff586
Mengyuan Mu <mu.mengyuan815@gmail.com>
2021-09-23 13:55:06 +1000

    backup

--- submit.sh
+++ submit.sh
@@ -3,13 +3,13 @@
 #PBS -m ae
 #PBS -P w35
 #PBS -q normalbw
-#PBS -l walltime=2:00:00
-#PBS -l mem=256GB
+#PBS -l walltime=12:00:00
+#PBS -l mem=128GB
 #PBS -l ncpus=1
 #PBS -j oe
 #PBS -l wd
 #PBS -l storage=gdata/w35
 
 #source activate science
-cd /g/data/w35/mm3972/scripts/process_data/make_LIS_landinfo
-ncl add_Copernicus_lai.ncl
+cd /g/data/w35/mm3972/scripts/process_data
+ncl regrid_to_10km.ncl
